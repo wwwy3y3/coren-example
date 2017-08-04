e053528d1ccfd20029fc188565601531a31086fc
JackHou <vjack070707@gmail.com>
2017-08-05 02:22:43 +0800

    fix bug & add css

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-
+import './home.css';
 export default class Root extends Component {
   render() {
     return (
-        <div>
+        <div className="home">
           Home Content
         </div>
     );
