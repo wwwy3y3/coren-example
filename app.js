7d3ec569624a0a81add50467ba383b24000be703
JackHou <vjack070707@gmail.com>
2017-08-21 19:58:58 +0800

    add route

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ app.set('port', (process.env.PORT || 9393));
 app.use(coren(__dirname));
 app.use('/dist', express.static(__dirname + '/.coren/public/dist'));
 
-app.get('/*', function(req, res) {
-  return res.sendCoren('index', {});
+app.get('/', function(req, res) {
+  return res.sendCoren('index');
+});
+
+app.get('/users', function(req, res) {
+  return res.sendCoren('index/users');
+});
+
+app.get('/users/:id', function(req, res) {
+  const id = req.params.id;
+  return res.sendCoren('index/users/' + id);
 });
 
 app.listen(app.get('port'), 'localhost', function(err) {
